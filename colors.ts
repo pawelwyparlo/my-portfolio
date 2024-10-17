c7f4ce70b59bde2f405809a4b3bed7ff9c549aa3
pawelwyparlo <pawel.wyparlo@montrosesoftware.com>
2024-10-17 22:45:21 +0200

    Start on header

---
+++ colors.ts
@@ -0,0 +1,9 @@
+export const grey = [
+  '#ffffff', // White
+  '#222222', // Black
+];
+
+export const blue = [
+  '#f5f5f5', // Light
+  '#0077FF', // Dark
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Layout } from 'antd';
+import { grey } from '../colors';
+import PageHeader from './components/PageHeader';
+
+const { Footer, Content } = Layout;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: grey[0],
+      }}
+    >
+      <PageHeader />
+      <Content></Content>
+      <Footer style={{ height: 20 }}></Footer>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
